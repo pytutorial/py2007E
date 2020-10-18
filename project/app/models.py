98dc682eae637463eaf9ae8179bfec34bed32783
Tung Duong <duongthanhtungvn01@gmail.com>
2020-10-18 15:16:52 +0100

    Update

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    code = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    code = models.CharField(max_length=30, unique=True)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=500, blank=True)
+    price = models.FloatField()
+    category = models.ForeignKey(Category,on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='static/images')
+    def __str__(self):
+        return self.name
--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,32 @@
 from django.shortcuts import render
 
+from .models import *
+
+priceList = [
+    {'id': 1, 'name': 'Dưới 5 triệu', 'max': 5},
+    {'id': 2, 'name': 'Từ 5 đến triệu', 'min': 5, 'max': 10},
+    {'id': 3, 'name': 'Trên 10 triệu', 'min': 10},
+]
+
 def index(request):
-    return render(request, 'index.html')
+    name = request.GET.get('name', '')
+    categoryId = request.GET.get('categoryId')    
+    productList = Product.objects.filter(name__icontains=name)    
+    categoryId = int(categoryId) if categoryId else None
+    if categoryId:
+        productList = productList.filter(category__id=categoryId)
+            
+    categoryList = Category.objects.all()
+    context = {
+        'priceList': priceList,
+        'categoryList': categoryList,
+        'productList': productList,
+        'name': name,
+        'categoryId': categoryId,
+    }
+    return render(request, 'index.html', context)
+
+def viewProductDetail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {'product': product}
+    return render(request, 'product.html', context)
