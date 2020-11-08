56d1e1a7a43af024cb0abe929acc0edbe00be7f4
pytutorial <dttvn0010@gmail.com>
2020-11-08 05:54:20 -0800

    Update

--- project/app/urls_staff.py
+++ project/app/urls_staff.py
@@ -2,8 +2,8 @@
 from django.urls import path
 from .views_staff import *
 urlpatterns = [
-  path('', staffHome, name='staff-home'),
-  path('list-category', listCategory, name='list-category'),
+  #path('', staffHome, name='staff-home'),
+  path('', listCategory, name='list-category'),
   path('create-category', CategoryCreateView.as_view(), name='create-category'),
   path('update-category/<pk>', CategoryUpdateView.as_view(), name='update-category'),
   path('delete-category/<pk>', deleteCategory, name='delete-category'),
